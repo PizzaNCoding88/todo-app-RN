890299298eb6d20dd17bb683e31d1b6bc119460b
PizzaNCoding88 <umbertonardiello@gmail.com>
2024-10-31 16:44:24 +0000

    added checkbox, to implement

--- components/Task.jsx
+++ components/Task.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Text, StyleSheet } from "react-native";
 import styles from "../styles/components styles/task";
+import CheckBox from "@react-native-community/checkbox";
 
 const Task = (props) => {
+  const [toggleCheckBox, setToggleCheckBox] = useState(false);
   const { task } = props;
-  return <Text style={styles.task}>{task}</Text>;
+  return (
+    <View>
+      <CheckBox
+        disabled={false}
+        value={toggleCheckBox}
+        onValueChange={(toggleCheckBox) => setToggleCheckBox(!toggleCheckBox)}
+      />
+      <Text style={styles.task}>{task}</Text>;
+    </View>
+  );
 };
 
 export default Task;
